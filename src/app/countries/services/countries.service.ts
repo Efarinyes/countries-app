f9b056f17394871371fd91b0edf6a8fbcaf8c21d
Eduard Farinyes <efarinyes@gmail.com>
2023-06-20 19:56:21 +0200

    Primer commit - app acabada

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, of, map} from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1'
+
+
+  constructor(private http: HttpClient) { }
+
+  searchCountryByAlpahaCode( code: string): Observable<Country | null > {
+
+    const url = `${ this.apiUrl }/alpha/${code}`
+    return this.http.get<Country[]>(url)
+      .pipe(
+        map( countries => countries.length > 0 ? countries[0] : null ),
+        catchError( () => of(null) )
+      )
+
+  }
+
+  searchCapital(term: string): Observable<Country[]> {
+
+    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`)
+      .pipe(
+        catchError( () => of([]) )
+      )
+  }
+  searchCountry(term: string): Observable<Country[]> {
+
+     return this.http.get<Country[]>(`${this.apiUrl}/name/${term}` )
+      .pipe(
+        catchError( () => of([]))
+      )
+
+  }
+
+  searchRegion(region: string): Observable<Country[]> {
+
+    return this.http.get<Country[]>(`${this.apiUrl}/region/${region}`)
+      .pipe(
+        catchError( () => of([]))
+      )
+  }
+
+}
